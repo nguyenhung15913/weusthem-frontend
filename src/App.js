5013487c7c0b30d1b6f519ba2ba97744cd4bbb0a
Hung <hnguyen97@myseneca.ca>
2022-07-26 13:55:08 -0600

    Finished

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+import Home from "./components/Home/Home";
+import ContactDetail from "./components/ContactDetail/ContactDetail";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
+import Contact from "./components/Contacts/Contact";
+import Nav from "./components/Nav/Nav";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const { user } = useContext(Context);
+
+	return (
+		<Router>
+			<Nav />
+			<Routes>
+				<Route exact path="/" element={user ? <Home /> : <Login />} />
+				<Route path="/login" element={user ? <Home /> : <Login />} />
+				<Route path="/register" element={user ? <Home /> : <Register />} />
+				<Route path="/contact" element={user ? <Contact /> : <Login />} />
+				<Route
+					path="/contact/:id"
+					element={user ? <ContactDetail /> : <Login />}
+				/>
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
